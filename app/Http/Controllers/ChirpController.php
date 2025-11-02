29ecdc447a12b4a39d20fcf7339635a22a94bcdb
keith409 <kitcruz409@gmail.com>
2025-11-02 18:26:22 +0800

    edit delete finish

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -33,7 +33,21 @@ public function index()
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+        'message' => 'required|string|max:255|min:5',
+    ], [
+        'message.required' => 'Please write something to chirp!',
+        'message.max' => 'Chirps must be 255 characters or less.',
+        'message.min' => 'Chirps must be at least 5 characters.',
+    ]);
+ 
+    \App\Models\Chirp::create([
+        'message' => $validated['message'],
+        'user_id' => null,
+    ]);
+ 
+    return redirect('/')->with('success', 'Your chirp has been posted!');
+    
     }
 
     /**
@@ -47,24 +61,37 @@ public function index()
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
-    {
-        //
-    }
+    
+public function edit(Chirp $chirp)
+{
+    // We'll add authorization in lesson 11
+    return view('chirps.edit', compact('chirp'));
+}
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+public function update(Request $request, Chirp $chirp)
+{
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+   
+    // Validate
+    $validated = $request->validate([
+        'message' => 'required|string|max:255',
+    ]);
+
+    // Update
+    $chirp->update($validated);
+
+    return redirect('/')->with('success', 'Chirp updated!');
+     
+    if ($request->user()->cannot('update', $chirp)) {
+        abort(403);
     }
+}
+
+public function destroy(Chirp $chirp)
+{
+    $chirp->delete();
+
+    return redirect('/')->with('success', 'Chirp deleted!');
+}
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,15 @@
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
-route::get('/', [ChirpController::class,'index']);
+ 
+Route::get('/', [ChirpController::class, 'index']);
+Route::post('/chirps', [ChirpController::class, 'store']);
+Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+
+/*
+Route::resource('/chirps', ChirpController::class)->only([
+    'index', 'store', 'edit', 'update', 'destroy'
+]);
+*/
